38c0ebc415a15af449866211f99f29bbd790ca4e
Colin Shaw <colin.shaw@aya.yale.edu>
2017-07-08 13:07:58 -0500

    Changed to Python 3

--- lesson-3/naive-bayes/prediction.py
+++ lesson-3/naive-bayes/prediction.py
@@ -3,27 +3,40 @@
 from classifier import GNB
 import json
 
+
+
 def main():
-	gnb = GNB()
-	with open('train.json', 'rb') as f:
-		j = json.load(f)
-	print j.keys()
-	X = j['states']
-	Y = j['labels']
-	gnb.train(X, Y)
 
-	with open('test.json', 'rb') as f:
-		j = json.load(f)
+   gnb = GNB()
 
-	X = j['states']
-	Y = j['labels']
-	score = 0
-	for coords, label in zip(X,Y):
-		predicted = gnb.predict(coords)
-		if predicted == label:
-			score += 1
-	fraction_correct = float(score) / len(X)
-	print "You got {} percent correct".format(100 * fraction_correct)
+   with open('train.json', encoding='utf-8') as f:
+       j = json.load(f)
+
+   print(j.keys())
+
+   X = j['states']
+   Y = j['labels']
+
+   gnb.train(X, Y)
+
+   with open('test.json', encoding='utf-8') as f:
+       j = json.load(f)
+
+   X = j['states']
+   Y = j['labels']
+
+   score = 0
+
+   for coords, label in zip(X,Y):
+       predicted = gnb.predict(coords)
+
+       if predicted == label:
+           score += 1
+
+   percent_correct = 100 * float(score) / len(X)
+   print(str(percent_correct) + " percent correct.")
+
+
 
 if __name__ == "__main__":
-	main()
+    main()
