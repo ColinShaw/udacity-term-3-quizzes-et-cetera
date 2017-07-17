26697aa9bf55b38eed7ca9cdbbc7a264b3d7a2ee
Colin Shaw <colin.shaw@aya.yale.edu>
2017-07-17 10:04:42 -0500

    Quintic implementation with fixed b[1] coefficient

---
+++ lesson-5/quintic-polynomial-python/README.md
@@ -0,0 +1,1 @@
+Goal is to implement the JMT(start, end, T) function in `quintic.py`.
--- lesson-5/quintic-polynomial-python/quintic.py
+++ lesson-5/quintic-polynomial-python/quintic.py
@@ -1,27 +1,33 @@
 import numpy as np
+from numpy.linalg import inv
 
 def JMT(start, end, T):
-    """
-    Calculate the Jerk Minimizing Trajectory that connects the initial state
-    to the final state in time T.
+    # The first three coefficients easily come from derivatives of s(t) evaluated at t=0
+    o1 = start[0]
+    o2 = start[1]
+    o3 = start[2] / 2
 
-    INPUTS
+    # Setting t=T in these derivatives gives us (including corrected b[1] equation)
+    A = [ [T**3,           T**4,       T**5],
+          [3 * T**2,   4 * T**3,   5 * T**4],
+          [6 * T,     12 * T**2,  20 * T**3] ]
+    b = [ end[0] - (start[0] + start[1] * T + 0.5 * start[2] * T**2),
+          end[1] - (start[1] + start[2] * T),
+          end[2] - start[2] ]
+    
+    # Make numpy arrays out of these so we get easy arithmetic
+    A = np.array(A)
+    b = np.array(b)
 
-    start - the vehicles start location given as a length three array
-        corresponding to initial values of [s, s_dot, s_double_dot]
+    # Compute x in Ax=b as defined above
+    x = inv(A).dot(b)
 
-    end   - the desired end state for vehicle. Like "start" this is a
-        length three array.
+    # Define the last three coefficients
+    o4 = x[0]
+    o5 = x[1]
+    o6 = x[2]
 
-    T     - The duration, in seconds, over which this maneuver should occur.
-
-    OUTPUT 
-    an array of length 6, each value corresponding to a coefficent in the polynomial 
-    s(t) = a_0 + a_1 * t + a_2 * t**2 + a_3 * t**3 + a_4 * t**4 + a_5 * t**5
-
-    EXAMPLE
-
-    > JMT( [0, 10, 0], [10, 10, 0], 1)
-    [0.0, 10.0, 0.0, 0.0, 0.0, 0.0]
-    """
-    return [1,2,3,4,5,6]
+    # Return the coefficient array
+    out = [o1, o2, o3, o4, o5, o6]
+    print(out)
+    return out
